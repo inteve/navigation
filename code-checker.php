1906c1a738b0ecf332042e77d3a501cb00f2d0f0
Jan Pecha <janpecha@email.cz>
2024-05-14 14:08:44 +0200

    Requires PHP 7.4 - 8.2

--- code-checker.php
+++ code-checker.php
@@ -1,7 +1,12 @@
 <?php
 
 return function (JP\CodeChecker\CheckerConfig $config) {
-	$config->setPhpVersion(new JP\CodeChecker\Version('5.6.0'));
+	$config->setPhpVersion(new JP\CodeChecker\Version('7.4.0'));
+	$config->setParameters([
+		'php' => [
+			'strictTypes' => FALSE,
+		],
+	]);
 	$config->addPath('./src');
 	$config->addPath('./tests');
 	JP\CodeChecker\Sets\CzProjectMinimum::configure($config);
